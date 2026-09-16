8fc5b1bfe1c4d8fe6cfcbdd8cf08b8da20d592e4
Miro Storm <mirostorm@gmail.com>
2026-09-16 15:02:05 +0200

    chore: Add home page

---
+++ apps/web/src/app/(user)/page.tsx
@@ -0,0 +1,10 @@
+export default function Index() {
+  return (
+    <div className="container">
+      <h1>
+        <span> Hello there, </span>
+        Welcome @itp-home-garden/web 👋
+      </h1>
+    </div>
+  );
+}
